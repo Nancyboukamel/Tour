d7b59aff2ebdd46c1411fb47fcc362946093b430
nancy <n.boukamel@zeroandone.me>
2018-05-08 23:04:53 +0300

    Adding divider

--- app/src/main/java/benkoreatech/me/tour/MapsActivity.java
+++ app/src/main/java/benkoreatech/me/tour/MapsActivity.java
@@ -23,7 +23,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -861,6 +863,7 @@ public class MapsActivity extends AppCompatActivity implements SearchView.OnQuer
             RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
             list_view.setLayoutManager(mLayoutManager);
             list_view.setItemAnimator(new DefaultItemAnimator());
+            list_view.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
             list_view.setAdapter(tourList);
         }
     }
